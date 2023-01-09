0a22b9abdb05a5b41926ce9352c26167aef43fef
Juan Daniel <juancastano0398@gmail.com>
2023-01-09 13:09:40 +0100

    actualizacion de mi back

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
+
+app.use(cors());
 
 app.post("/imagen", (req, res) => {
   const suma = 5 + 5;
 
   const resultado = suma * 80;
 
-  const resultadoFinal = res.status(200).json({ msg: "Tu imagen es cafe" });
+  const resultadoFinal = "Torta de cafe con la suma de" + resultado / 2;
+
+  res.status(200).json({ resultadoFinal });
+});
+
+app.post("/login", (req, res) => {
+  res.status(200).json({ msg: "Iniciaste sesion correctamente" });
 });
 
 const PORT = 4040;
